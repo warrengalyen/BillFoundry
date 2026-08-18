f85e2c521e1654b09108d5b2a3797adcb3900a51
warrengalyen <wgalyen@hotmail.com>
2026-08-18 15:33:06 -0700

    fix(a11y): restore focus after closing mobile navigation

--- src/BillFoundry.Web/Components/Layout/NavMenu.razor.cs
+++ src/BillFoundry.Web/Components/Layout/NavMenu.razor.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 
 namespace BillFoundry.Web.Components.Layout;
@@ -5,16 +6,35 @@ namespace BillFoundry.Web.Components.Layout;
 public partial class NavMenu
 {
     private bool _navOpen;
+    private ElementReference _navToggle;
 
-    private void ToggleNav() => _navOpen = !_navOpen;
+    private async Task ToggleNavAsync()
+    {
+        _navOpen = !_navOpen;
+        if (!_navOpen)
+        {
+            await _navToggle.FocusAsync();
+        }
+    }
 
     private void CloseNav() => _navOpen = false;
 
-    private void OnNavKeyDown(KeyboardEventArgs args)
+    private async Task DismissNavAsync()
+    {
+        if (!_navOpen)
+        {
+            return;
+        }
+
+        _navOpen = false;
+        await _navToggle.FocusAsync();
+    }
+
+    private async Task OnNavKeyDown(KeyboardEventArgs args)
     {
         if (_navOpen && string.Equals(args.Key, "Escape", StringComparison.Ordinal))
         {
-            CloseNav();
+            await DismissNavAsync();
         }
     }
 }
